df8a3386858bbfeff5621cfca1704a77d8e641c7
Reme Le Hane <mail@remej.dev>
2022-06-05 11:47:58 +0200

    fix(api): updated auth response codes

--- api/src/middleware/auth.middleware.ts
+++ api/src/middleware/auth.middleware.ts
@@ -3,11 +3,11 @@ import jwt from 'jsonwebtoken'
 export default (req, res, next) => {
   try {
     const token = req.header('x-auth-token')
-    if (!token) return res.status(403).send('Access denied.')
+    if (!token) return res.send('Access denied.', 403)
     
     req.user = jwt.verify(token, process.env.JWTPRIVATEKEY)
     next()
   } catch (_) {
-    res.status(400).send('Invalid token')
+    res.status(403).send('Invalid token')
   }
 }
