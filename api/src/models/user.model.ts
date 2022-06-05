8507d0cd514b18115bc1763f13b7641134efbe27
Reme Le Hane <mail@remej.dev>
2022-06-05 11:27:16 +0200

    fix(api): setup defaults for older data structures

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -7,10 +7,12 @@ const userSchema = new Schema({
   firstName: {
     type: String,
     required: true,
+    default: '',
   },
   lastName: {
     type: String,
     required: true,
+    default: '',
   },
   username: {
     type: String,
