19b21819303c1e12ced40edb9d1578de71e85591
Reme Le Hane <mail@remej.dev>
2022-06-05 11:27:57 +0200

    fix(ui): updated auth guard with utils to read token and load user into memory

--- ui/lib/router/auth_guard.dart
+++ ui/lib/router/auth_guard.dart
@@ -1,7 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:weight_tracker/core/constants.dart';
-import 'package:weight_tracker/locator.dart';
 import 'package:weight_tracker/screens/login/utils/auth_utils.dart';
 
 import 'app_router.gr.dart';
@@ -16,7 +13,7 @@ class AuthGuard extends AutoRouteGuard {
     // We use a simple check to see if an access token exists and "assume"
     // that the user is logged in, the [NetworkManager] will handle the de-authorization
     // when the API returns a 401
-    final token = await sl<FlutterSecureStorage>().read(key: SECURE_API_KEY);
+    final token = await AuthUtils.fetchToken();
     final isAuthed = token != null && token.isNotEmpty;
     if (isAuthed) {
       // if user is authenticated we continue
