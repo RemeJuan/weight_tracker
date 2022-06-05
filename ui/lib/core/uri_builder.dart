55e0d90ece2e382410a593d01a5f7039a954a496
Reme Le Hane <mail@remej.dev>
2022-06-05 10:28:24 +0200

    fix(ui): updated uri builder

--- ui/lib/core/uri_builder.dart
+++ ui/lib/core/uri_builder.dart
@@ -4,7 +4,8 @@ class UriBuilder {
   Uri uri(String path, {Map<String, dynamic>? queryParameters}) {
     return Uri(
       scheme: 'http',
-      host: const String.fromEnvironment('API'),
+      host: const String.fromEnvironment('API').split(":").first,
+      port: int.parse(const String.fromEnvironment('API').split(":").last),
       path: "api/$path",
       queryParameters: queryParameters,
     );
