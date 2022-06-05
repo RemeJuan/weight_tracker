0a693f4694c295bb3d50afbed43616104902bb8b
Reme Le Hane <mail@remej.dev>
2022-06-05 11:28:51 +0200

    feat(ui): added util for reading token

--- ui/lib/screens/login/utils/auth_utils.dart
+++ ui/lib/screens/login/utils/auth_utils.dart
@@ -6,7 +6,7 @@ import 'package:weight_tracker/locator.dart';
 import 'package:weight_tracker/models/user_model.dart';
 
 class AuthUtils {
-  static void loadUserIntoMemory() async {
+  static Future<void> loadUserIntoMemory() async {
     final user = await sl<FlutterSecureStorage>().read(key: SECURE_USER_KEY);
 
     if (user != null) {
@@ -19,7 +19,7 @@ class AuthUtils {
     }
   }
 
-  static void storeUser(UserModel user) async {
+  static Future<void> storeUser(UserModel user) async {
     await sl<FlutterSecureStorage>().write(
       key: SECURE_USER_KEY,
       value: jsonEncode({
@@ -30,10 +30,14 @@ class AuthUtils {
     );
   }
 
-  static void storeToken(UserModel user) async {
+  static Future<void> storeToken(UserModel user) async {
     await sl<FlutterSecureStorage>().write(
       key: SECURE_API_KEY,
       value: user.token,
     );
+  }
+
+  static Future<String?> fetchToken() async {
+    return await sl<FlutterSecureStorage>().read(key: SECURE_API_KEY);
   }
 }
